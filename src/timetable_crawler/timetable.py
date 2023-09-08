b909a85e4289dc546441b962f6a527b610d55635
durachok <fenixrnd@mail.ru>
2023-09-08 16:05:47 +0700

    current extension initial code

--- src/timetable_crawler/timetable.py
+++ src/timetable_crawler/timetable.py
@@ -1,3 +1,4 @@
+import json
 from dataclasses import dataclass
 from enum import IntEnum, auto
 from typing import Tuple, Optional
@@ -33,3 +34,26 @@ class Day(dataclass_wizard.JSONWizard):
 class Timetable(dataclass_wizard.JSONWizard):
     times: Tuple[str, ...]
     days: Tuple[Day, ...]
+
+
+if __name__ == "__main__":
+    import random
+
+
+    # Generate random data for the Timetable object
+    def generate_random_subject():
+        name = random.choice(['Math', 'Science', 'English'])
+        teacher = random.choice(['Mr. Smith', 'Mrs. Johnson', 'Ms. Thompson'])
+        place = random.choice(['Room 101', 'Room 202', 'Room 303'])
+        periodicity = random.choice(list(Periodicity))
+        is_empty = bool(random.getrandbits(1))
+        return Subject(name=name, teacher=teacher, place=place, periodicity=periodicity, is_empty=is_empty)
+
+
+    times = ('9:00', '10:00', '11:00')
+    days = (
+        Day('Monday', tuple([generate_random_subject() for i in range(10)])),
+    )
+
+    t = Timetable(times, days)
+    print(t.to_json())
