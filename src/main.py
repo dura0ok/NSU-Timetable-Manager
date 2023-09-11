9d857bb220dd2db0a9fca1b771ef4454b9ec602f
Kirill Kondrenko <kondrenkokp@yandex.ru>
2023-09-11 10:20:53 +0700

    current state xD

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
-from flask import Flask
-from timetable_crawler import get_timetable
+#from flask import Flask
+from bs4 import BeautifulSoup
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 
-@app.route('/<group_id>')
-def hello_world(group_id):
-    return get_timetable(group_id)
+# @app.route('/<group_id>')
+# def hello_world(group_id):
+#     return get_timetable(group_id)
 
 
 if __name__ == '__main__':
-    app.run()
+    f = open('index.html')
+    s = f.read()
+
+    soup = BeautifulSoup(s, 'html.parser')
+    print(soup.find("table", {"class": "time-table"}))
