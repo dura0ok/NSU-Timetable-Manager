1fec6572c8aff5ff68cf7ab507bc3fdd48978734
Kirill Kondrenko <kondrenkokp@yandex.ru>
2023-10-22 15:19:47 +0700

    Attemtp №2

--- src/server/main.py
+++ src/server/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 from extractor import Extractor
 from html_extraction import HTMLExtractor
@@ -14,21 +14,25 @@ CORS(app)
 extractor: Extractor = HTMLExtractor()
 
 
+@cross_origin()
 @app.route('/timetable/<group_id>')
 def get_timetable(group_id: str) -> str:
     return extractor.extract_timetable(group_id).to_json()
 
 
+@cross_origin()
 @app.route('/room/<room_name>')
 def get_room(room_name: str) -> str:
     return extractor.extract_room(split_url_word(room_name)).to_json()
 
 
+@cross_origin()
 @app.route('/tutor/<tutor_name>')
 def get_tutor(tutor_name: str) -> str:
     return extractor.extract_tutor(split_url_word(tutor_name)).to_json()
 
 
+@cross_origin()
 @app.route('/times')
 def get_times() -> str:
     return extractor.extract_times().to_json()
