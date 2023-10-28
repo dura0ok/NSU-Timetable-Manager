2fdcf46c0d81b2afa2e9dd47f6947af0347eab1c
Kirill Kondrenko <kondrenkokp@yandex.ru>
2023-10-28 16:10:29 +0700

    Added HTTP-escaping

--- src/server/main.py
+++ src/server/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
+from markupsafe import escape
 
 from extractor import Extractor
 from html_extraction import HTMLExtractor
@@ -17,19 +18,19 @@ extractor: Extractor = HTMLExtractor()
 @cross_origin()
 @app.route('/timetable/<group_id>')
 def get_timetable(group_id: str) -> str:
-    return extractor.extract_timetable(group_id).to_json()
+    return extractor.extract_timetable(escape(group_id)).to_json()
 
 
 @cross_origin()
 @app.route('/room/<room_name>')
 def get_room(room_name: str) -> str:
-    return extractor.extract_room(split_url_word(room_name)).to_json()
+    return extractor.extract_room(split_url_word(escape(room_name))).to_json()
 
 
 @cross_origin()
 @app.route('/tutor/<tutor_name>')
 def get_tutor(tutor_name: str) -> str:
-    return extractor.extract_tutor(split_url_word(tutor_name)).to_json()
+    return extractor.extract_tutor(split_url_word(escape(tutor_name))).to_json()
 
 
 @cross_origin()
