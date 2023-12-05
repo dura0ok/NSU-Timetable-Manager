d1a5a258c913c3c6cc8e936c7a4b402b20e6707b
Kirill Kondrenko <kondrenkokp@yandex.ru>
2023-12-05 22:54:59 +0700

    Minor fixes in server)

--- src/server/config_parsing/env_config_parser.py
+++ src/server/config_parsing/env_config_parser.py
@@ -10,12 +10,15 @@ class EnvConfigParser(ConfigParser):
     __host_key_name: str = 'HOST'
     __port_key_name: str = 'PORT'
 
+    __default_host: str = '127.0.0.1'
+    __default_port: int = 8000
+
     def __init__(self) -> None:
         load_dotenv()
 
-    def parse_host(self, def_value: str = '127.0.0.1') -> str:
-        return os.getenv(EnvConfigParser.__host_key_name) or def_value
+    def parse_host(self) -> str:
+        return os.getenv(EnvConfigParser.__host_key_name) or self.__default_host
 
-    def parse_port(self, def_value: int = 8000) -> int:
+    def parse_port(self) -> int:
         port: Optional[str] = os.getenv(EnvConfigParser.__port_key_name)
-        return int(port) if port is not None else def_value
+        return int(port) if port is not None else self.__default_port
