5efcc5e1e4b3cc6a2db1148f5ad815eba3cd0373
Kirill Kondrenko <kondrenkokp@yandex.ru>
2023-12-06 14:04:49 +0700

    Minor fix to server

--- src/server/services/extraction/html_extraction/downloading/html_downloader.py
+++ src/server/services/extraction/html_extraction/downloading/html_downloader.py
@@ -4,11 +4,16 @@ from .html_downloading_exception import HTMLDownloadingException
 
 
 class HTMLDownloader:
+    __timeout: int = 5  # In seconds
+
     @staticmethod
     def download(url: str) -> str:
-        response: requests.Response = requests.get(url)
-
-        if not response.ok:
-            raise HTMLDownloadingException('Cannot download HTML')
+        try:
+            response: requests.Response = requests.get(url, timeout=HTMLDownloader.__timeout)
+        except (requests.ReadTimeout, requests.ConnectionError) as e:
+            raise HTMLDownloadingException(str(e))
+        else:
+            if not response.ok:
+                raise HTMLDownloadingException('Cannot download HTML')
 
         return response.text
